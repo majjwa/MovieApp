68d6e72f2788d6449c68846a81ddb174665e5777
Marwa Maky <marwamaky50@gmail.com>
2024-09-26 23:59:23 +0300

    Project Initial Creation
    Project Initial Creation

---
+++ banquemisr.challenge05/banquemisr.challenge05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  banquemisr.challenge05
+//
+//  Created by marwa maky on 26/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
